bcd09e2e24112d3a20150114f24f52b4db380140
nngedeon <gedeonkira@gmail.com>
2025-01-28 17:43:23 +0300

    commit on main branch

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CustomersPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Customers</h1>
+            <p>Welcome to the customers page.</p>
+            {/* Add your customer-related content here */}
+        </div>
+    );
+};
+
+export default CustomersPage;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import {Inter}  from "next/font/google"
+import {Roboto}  from "next/font/google"
+import {Lusitana}  from "next/font/google"
+export const inter= Inter({
+    subsets: ['latin'],
+})
+export const lusitana= Lusitana({
+    weight: ['400','700'],
+    subsets: ['latin'],
+})
+export const roboto= Roboto({
+    weight: ['100','300','900'],
+    subsets: ['latin'],
+})
